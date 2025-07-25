9a099a10182ea86ba04bd22029359731a7c1a021
Dmitry Ponyatov <dponyatov@gmail.com>
2025-07-26 02:28:27 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 #![allow(unused_variables)]
 
+use memmap2::Mmap;
+use std::fs::File;
+
 fn main() {
     let argv: Vec<String> = std::env::args().collect();
     let argc = argv.len();
     arg(0, &argv[0]);
     for (argc, argv) in argv.iter().enumerate().skip(1) {
         arg(argc, argv);
+        let file = File::open(argv).unwrap();
+        let mmap = unsafe { Mmap::map(&file).unwrap() };
     }
 }
 

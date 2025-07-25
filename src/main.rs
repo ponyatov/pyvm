ac4ed8b71197375525129390b78d43601ccf0597
Dmitry Ponyatov <dponyatov@gmail.com>
2025-07-26 02:24:39 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,14 @@
-fn main() { println!("Hello, world!"); }
+#![allow(unused_variables)]
+
+fn main() {
+    let argv: Vec<String> = std::env::args().collect();
+    let argc = argv.len();
+    arg(0, &argv[0]);
+    for (argc, argv) in argv.iter().enumerate().skip(1) {
+        arg(argc, argv);
+    }
+}
+
+fn arg(argc: usize, argv: &str) {
+    eprintln!("argv[{argc}] = {argv:?}");
+}

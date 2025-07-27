ce33c2aa1b2aad61f56425679a87e7c9aa6482bb
Dmitry Ponyatov <dponyatov@gmail.com>
2025-07-27 14:39:21 +0400

    .

--- src/server.rs
+++ src/server.rs
@@ -14,16 +14,15 @@ const HTTP_200_OK: &[u8] = b"HTTP/1.1 200 OK\r\n";
 const HTTP_404_NOTFOUND: &[u8] = b"HTTP/1.1 404 Not Found\r\n";
 
 // MIME types
-const TEXT_HTML: &[u8] = b"Content-Type: text/html\r\n";
-const TEXT_PLAIN: &[u8] = b"Content-Type: text/plain\r\n";
+const TEXT_HTML: &[u8] = b"Content-Type: text/html; charset=utf-8\r\n";
+const TEXT_PLAIN: &[u8] = b"Content-Type: text/plain; charset=utf-8\r\n";
 const TEXT_CSS: &[u8] = b"Content-Type: text/css\r\n";
 const TEXT_JS: &[u8] = b"Content-Type: application/javascript\r\n";
 const IMAGE_PNG: &[u8] = b"Content-Type: image/png\r\n";
 
 // jigs
-// const HTTP_KEEP_ALIVE: &[u8] = b"Connection: keep-alive\r\nKeep-Alive: timeout=5, max=1000\r\n";
-const HTTP_CACHE: &[u8] = b"Cache-Control: public, max-age=5\r\n";
-const HTTP_IMMUTABLE: &[u8] = b"Cache-Control: immutable\r\n";
+const HTTP_CACHE: &[u8] = b"Cache-Control: public, max-age=3600\r\n";
+const HTTP_IMMUTABLE: &[u8] = b"Cache-Control: immutable, max-age=3600\r\n";
 const HTTP_NOCACHE: &[u8] = b"Cache-Control: no-cache\r\n";
 
 // static content
@@ -35,19 +34,15 @@ const CSS_CSS: &[u8] = include_bytes!("../static/css.css");
 const JS_JS: &[u8] = include_bytes!("../static/js.js");
 const JQUERY_MIN_JS: &[u8] = include_bytes!("../static/cdn/jquery.min.js");
 
-fn error_404(client: &mut TcpStream, method: &[u8], url: &[u8], request: &[u8]) {
+fn error_404(client: &mut TcpStream, request: &[u8]) {
     client.write(&HTTP_404_NOTFOUND).unwrap();
     client.write(&TEXT_HTML).unwrap();
+    client.write(&HTTP_NOCACHE).unwrap();
     client.write(b"\r\n").unwrap();
     client.write(&INDEX_HEAD).unwrap();
     client.write(b"<pre>\r\n").unwrap();
-    // client.write(b"\r\n<pre>\r\nmethod: ").unwrap();
-    // client.write(method).unwrap();
-    // client.write(b" ").unwrap();
-    // client.write(url).unwrap();
-    // client.write(b"\r\n\r\n").unwrap();
     client.write(request).unwrap();
-    client.write(b"</pre>\r\n").unwrap();
+    client.write(b"</pre></body></html>\r\n").unwrap();
 }
 
 fn index(client: &mut TcpStream) {
@@ -79,6 +74,22 @@ fn css(client: &mut TcpStream) {
     client.flush().unwrap();
 }
 
+fn js(client: &mut TcpStream) {
+    client.write(&HTTP_200_OK).unwrap();
+    client.write(&TEXT_JS).unwrap();
+    client.write(&HTTP_CACHE).unwrap();
+    client.write(b"\r\n").unwrap();
+    client.write(&JS_JS).unwrap();
+}
+
+fn jquery(client: &mut TcpStream) {
+    client.write(&HTTP_200_OK).unwrap();
+    client.write(&TEXT_JS).unwrap();
+    client.write(&HTTP_IMMUTABLE).unwrap();
+    client.write(b"\r\n").unwrap();
+    client.write(&JQUERY_MIN_JS).unwrap();
+}
+
 fn router(client: &mut TcpStream) {
     let mut request = [0; 1024];
     client.read(&mut request).unwrap();
@@ -88,12 +99,12 @@ fn router(client: &mut TcpStream) {
     let (method, url) = (parts[0], parts[1]);
 
     match (method, url) {
-        // (b"GET", b"/") | (b"GET", b"/index.html") => index(client),
+        (b"GET", b"/") | (b"GET", b"/index.html") => index(client),
         (b"GET", b"/favicon.ico") | (b"GET", b"/logo.png") => logo(client),
         (b"GET", b"/css.css") => css(client),
-        // (b"GET", b"/js.js") => js(client),
-        // (b"GET", b"/jquery.min.js") => jquery(client),
-        _ => error_404(client, method, url, &request),
+        (b"GET", b"/jquery.min.js") => jquery(client),
+        (b"GET", b"/js.js") => js(client),
+        _ => error_404(client, &request),
     }
 }
 

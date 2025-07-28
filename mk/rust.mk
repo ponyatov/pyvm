dbf70864e50b226cc334becdbca60adc144991e7
Dmitry Ponyatov <dponyatov@gmail.com>
2025-07-28 10:14:08 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/rust.mk
+++ mk/rust.mk
@@ -1,17 +1,17 @@
 RTARGET = x86_64-unknown-linux-gnu
 # RTARGET = aarch64-unknown-linux-gnu
 # RTARGET = i686-pc-windows-gnu
-# RTARGET = wasm32-unknown-unknown
 
 $(RUSTUP) $(CARGO):
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-	rustup target add x86_64-unknown-linux-gnu
+	rustup target add $(RTARGET)
 	rustup target add wasm32-unknown-unknown
 	rustup component add rust-analyzer rustfmt
 	cargo  install cargo-watch cargo-binutils
 # rustup target add x86_64-unknown-linux-gnu
 # rustup target add aarch64-unknown-linux-gnu
 # rustup target add i686-pc-windows-gnu
+# rustup target add wasm32-unknown-unknown
 
 .PHONY: server
 server: $(R)
